0070c966b32072a2cb8c29113642602b433449df
ENZO VENTURI <enzo_venturi@estudante.sesisenai.org.br>
2026-08-17 18:34:34 -0300

    Creating classes

---
+++ src/main/java/org/example/pokedex/controller/GameController.java
@@ -0,0 +1,31 @@
+package org.example.pokedex.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/pokedex")
+public class GameController {
+    //public String move() {
+    //    return "MOVED";
+    //}
+//
+    //public String capture(Pokemon pokemon) {
+    //    return "Captured: " + pokemon;
+    //}
+//
+    //public String battle(Pokemon battleStarter, Pokemon adversary) {
+    //    return "POKEMON STARTER: " + battleStarter + " VS ADVERSARY: " + adversary;
+    //}
+//
+    //public String buyItem(String item) {
+    //    return "Bought item: " + item;
+    //}
+    //
+    //public String heal(double health, Pokemon pokemon) {
+    //    return "HEALED " + health + " POKEMON: " + pokemon;
+    //}
+
+
+
+}
---
+++ src/main/java/org/example/pokedex/service/PokemonService.java
@@ -0,0 +1,7 @@
+package org.example.pokedex.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PokemonService {
+}
